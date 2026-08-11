fba3cc97570eee04d9aebadcb71652c4cb2ad57b
Ranfees <153532316+Ranfees@users.noreply.github.com>
2026-08-11 21:07:39 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-function Card({title}){
+function Card({ title }) {
 
-const [liked,setLiked]= useState(false);
+  const [liked, setLiked] = useState(false);
 
-useEffect(()=>{
-  console.log(`${title}has liked:${liked}`)
-})
+  const [count, setCount] = useState(0);
 
-  return(
-    <div className='movie-card'>
-      <h2>{title}</h2>
-      <button onClick={()=>{setLiked(!liked)}}> {liked? 'liked':'like'}</button>
+  useEffect(() => {
+    console.log(`${title}has liked:${liked}`)
+  },[liked])
+
+  return (
+    <div className='movie-card' onClick={() => {
+      setCount(count + 1)
+    }}>
+      <h2>{title}<br />{count}</h2>
+      <button onClick={() => { setLiked(!liked) }}> {liked ? 'liked' : 'like'}</button>
     </div>
   )
 }
 
-function Card2({title}){
-  return(
+function Card2({ title }) {
+  return (
     <div className='movie-card'>
       <h2>{title}</h2>
     </div>
   )
 }
-function App (){
-  return(
+function App() {
+  return (
     <div className='card'>
       <div>
-        <Card title="avatar"/>
-      <Card title="sambar"/>
-      <Card title="end game"/>
-      <Card title="spiderman"/>
+        <Card title="avatar" />
+        <Card title="sambar" />
+        <Card title="end game" />
+        <Card title="spiderman" />
 
       </div>
       <div>
-<Card2 title="avatar"/>
-      <Card2 title="sambar"/>
-      <Card2 title="end game"/>
-      <Card2 title="spiderman"/>
+        <Card2 title="avatar" />
+        <Card2 title="sambar" />
+        <Card2 title="end game" />
+        <Card2 title="spiderman" />
       </div>
     </div>
   )
