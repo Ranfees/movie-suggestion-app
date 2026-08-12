52f8fb7dac70c3ac8cf6116953320687e2eeb22f
Ranfees <153532316+Ranfees@users.noreply.github.com>
2026-08-12 23:30:59 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,11 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 
-function Card({ title }) {
-
-  const [liked, setLiked] = useState(false);
-
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`${title}has liked:${liked}`)
-  },[liked])
-
-  return (
-    <div className='movie-card' onClick={() => {
-      setCount(count + 1)
-    }}>
-      <h2>{title}<br />{count}</h2>
-      <button onClick={() => { setLiked(!liked) }}> {liked ? 'liked' : 'like'}</button>
-    </div>
-  )
-}
-
-function Card2({ title }) {
-  return (
-    <div className='movie-card'>
-      <h2>{title}</h2>
-    </div>
-  )
-}
 function App() {
   return (
-    <div className='card'>
-      <div>
-        <Card title="avatar" />
-        <Card title="sambar" />
-        <Card title="end game" />
-        <Card title="spiderman" />
-
-      </div>
-      <div>
-        <Card2 title="avatar" />
-        <Card2 title="sambar" />
-        <Card2 title="end game" />
-        <Card2 title="spiderman" />
-      </div>
-    </div>
+    <h1>
+  Find <span className="text-gradient">Movies</span> You'll Enjoy
+  Without the Hassle
+</h1>
   )
 }
 
